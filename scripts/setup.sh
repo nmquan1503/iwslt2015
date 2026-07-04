c8ccb5af04bc2f04073fe6bd8bd1bec6c32e5010
nmquan1503 <minhquan15032005@gmail.com>
2026-07-04 20:58:03 +0700

    fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@ pip install setuptools
 pip install sacrebleu
 pip install rouge-score
 
-git clone https://github.com/nmquan1503/attention.git -b dev -q
+git clone https://github.com/nmquan1503/attention.git -b main -q
 cd attention
 pip install . --no-build-isolation -q
 cd ..
