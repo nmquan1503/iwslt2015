ba2f97b4476b27ddc28385946e9c00fed54f9649
nmquan1503 <minhquan15032005@gmail.com>
2026-07-12 01:23:46 +0700

    new

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,1 +1,2 @@
+export HF_HUB_DISABLE_XET=1
 python -m evaluation.evaluate
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,12 @@
 pip install setuptools
 pip install sacrebleu
 pip install rouge-score
+pip uninstall -y \
+    unbabel-comet \
+    transformers \
+    tokenizers \
+    sentencepiece \
+    entmax
 pip install unbabel-comet
 
 git clone https://github.com/nmquan1503/selective-attention.git -b dev -q
